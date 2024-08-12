9b919d9bc9c6140eabb34c8b10b9037b2a0b4573
trashpirate <nadinaoates@gmail.com>
2024-08-11 22:50:44 -0500

    some test

--- test/Test.t.sol
+++ test/Test.t.sol
@@ -5,4 +5,8 @@ import {Test, console} from "forge-std/Test.sol";
 
 contract TestScript is Test {
     function setUp() external virtual {}
+
+    function test__test() public {
+        console.log("This is a test");
+    }
 }
